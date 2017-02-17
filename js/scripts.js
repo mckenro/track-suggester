99bad6598de358cfe053b2371a9d27edd58af265
Rob McKenzie <rob@>
2017-02-17 09:32:17 -0800

    add updated form input names

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $("#form...").submit(function(event) {
     event.preventDefault();
-    
+    var frontBackInput = $("input:radio[name=frontBack]:checked").val();
   });
 });
