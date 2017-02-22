53bffa2be861b3cbc9351b9dfbe2453cd0c92056
Rob McKenzie <mckenro@gmail.com>
2017-02-21 20:48:42 -0800

    add code to return to using radio button input and update scripts.js to funtion with radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,48 @@
 $(document).ready(function() {
-/*
-  $("button#userName").click(function() {
-    $("#frontBack").show();
-    $("#userName").hide();
-  });
 
-  $("button#frontBackButton").click(function() {
-    $("#appType").show();
-    $("#frontBack").hide();
-  });
-  $("button#appTypeButton").click(function() {
-    $("#techType").show();
-    $("#appType").hide();
-  });
-  $("button#techTypeButton").click(function() {
-    $("#coType").show();
-    $("#techType").hide();
-  });
-  $("button#coTypeButton").click(function() {
-    $("#coSpecific").show();
-    $("#coType").hide();
-  });
-  $("button#coSpecificButton").click(function() {
-    $("#resultField").show();
-    $("#coSpecific").hide();
-  });
-*/
   $("form#suggesterQuiz").submit(function(event) {
     event.preventDefault();
 
-    var userNameInput = $("#userName").val();
-    var frontBackInput = $("select#frontBack").val();
-    var appTypeInput = $("#appType").val();
-    var techTypeInput = $("#techType").val();
-    var coTypeInput = $("#coType").val();
-    var coSpecificInput = $("#coSpecific").val();
+    var userNameInput = $("input#userName").val();
+    var frontBackInput = $("input:radio[name=frontBack]:checked").val();
+    var appTypeInput = $("input:radio[name=appType]:checked").val();
+    var techTypeInput = $("input:radio[name=techType]:checked").val();
+     var coTypeInput = $("input:radio[name=coType]:checked").val();
+     var coSpecificInput = $("input:radio[name=coSpecific]:checked").val();
 
-    if ((frontBackInput === "1" && coSpecificInput === "4") || (frontBackInput === "1" && coSpecificInput === "5")) {
+    if (!userNameInput || !frontBackInput || !appTypeInput || !techTypeInput || !coTypeInput || !coSpecificInput){
+      alert("Please choose an answer for all questions!");
+    }else if ((frontBackInput === "1" && coSpecificInput === "4") || (frontBackInput === "1" && coSpecificInput === "5")) {
+      $("#cSharp").hide();
+      $("#phpDrupal").hide();
+      $("#rubyRails").hide();
+      $("#moreInfo").hide();
       $("#cssDesign").show();
     }else if ((frontBackInput === "2" && appTypeInput === "2") || (frontBackInput === "2" && techTypeInput === "1") || (frontBackInput === "2" && coSpecificInput === "5")) {
+      $("#cSharp").hide();
+      $("#phpDrupal").hide();
+      $("#cssDesign").hide();
+      $("#moreInfo").hide();
       $("#rubyRails").show();
     }else if ((frontBackInput === "2" && coSpecificInput === "1") || (frontBackInput === "2" && techTypeInput === "2") || (techTypeInput === "2" && coSpecificInput === "1")) {
+      $("#cssDesign").hide();
+      $("#phpDrupal").hide();
+      $("#rubyRails").hide();
+      $("#moreInfo").hide();
       $("#cSharp").show();
     }else if ((appTypeInput === "2" && techTypeInput === "1") || (appTypeInput === "2" && coSpecificInput === "5")) {
+      $("#cSharp").hide();
+      $("#cssDesign").hide();
+      $("#rubyRails").hide();
+      $("#moreInfo").hide();
       $("#phpDrupal").show();
     }else {
+      $("#cSharp").hide();
+      $("#phpDrupal").hide();
+      $("#rubyRails").hide();
+      $("#cssDesign").hide();
       $("#moreInfo").show();
     }
-    alert(userNameInput, frontBackInput);
   });
 });
 
