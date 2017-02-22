23538530a4edeed29b0a5ff782441515204f29fb
Rob McKenzie <mckenro@gmail.com>
2017-02-21 21:04:22 -0800

    add styling for results area

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,8 @@ $(document).ready(function() {
     var frontBackInput = $("input:radio[name=frontBack]:checked").val();
     var appTypeInput = $("input:radio[name=appType]:checked").val();
     var techTypeInput = $("input:radio[name=techType]:checked").val();
-     var coTypeInput = $("input:radio[name=coType]:checked").val();
-     var coSpecificInput = $("input:radio[name=coSpecific]:checked").val();
+    var coTypeInput = $("input:radio[name=coType]:checked").val();
+    var coSpecificInput = $("input:radio[name=coSpecific]:checked").val();
 
     if (!userNameInput || !frontBackInput || !appTypeInput || !techTypeInput || !coTypeInput || !coSpecificInput){
       alert("Please choose an answer for all questions!");
@@ -17,36 +17,36 @@ $(document).ready(function() {
       $("#phpDrupal").hide();
       $("#rubyRails").hide();
       $("#moreInfo").hide();
+      $("#resultField").show();
       $("#cssDesign").show();
     }else if ((frontBackInput === "2" && appTypeInput === "2") || (frontBackInput === "2" && techTypeInput === "1") || (frontBackInput === "2" && coSpecificInput === "5")) {
       $("#cSharp").hide();
       $("#phpDrupal").hide();
       $("#cssDesign").hide();
       $("#moreInfo").hide();
+      $("#resultField").show();
       $("#rubyRails").show();
     }else if ((frontBackInput === "2" && coSpecificInput === "1") || (frontBackInput === "2" && techTypeInput === "2") || (techTypeInput === "2" && coSpecificInput === "1")) {
       $("#cssDesign").hide();
       $("#phpDrupal").hide();
       $("#rubyRails").hide();
       $("#moreInfo").hide();
+      $("#resultField").show();
       $("#cSharp").show();
     }else if ((appTypeInput === "2" && techTypeInput === "1") || (appTypeInput === "2" && coSpecificInput === "5")) {
       $("#cSharp").hide();
       $("#cssDesign").hide();
       $("#rubyRails").hide();
       $("#moreInfo").hide();
+      $("#resultField").show();
       $("#phpDrupal").show();
     }else {
       $("#cSharp").hide();
       $("#phpDrupal").hide();
       $("#rubyRails").hide();
       $("#cssDesign").hide();
+      $("#resultField").show();
       $("#moreInfo").show();
     }
   });
 });
-
-
-/*else if (!userNameInput || !frontBackInput || !appTypeInput || !techTypeInput || !coTypeInput || !coSpecificInput){
-  alert("Please choose an answer for all questions!");
-}*/
