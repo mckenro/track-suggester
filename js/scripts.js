0c0a432d509543973e5bbe0ea655558e39a653bb
Rob McKenzie <mckenro@gmail.com>
2017-02-22 17:22:50 -0800

    add styling to display area- this includes hiding the survey questions when input is submitted

--- js/scripts.js
+++ js/scripts.js
@@ -13,34 +13,39 @@ $(document).ready(function() {
     if (!userNameInput || !frontBackInput || !appTypeInput || !techTypeInput || !coTypeInput || !coSpecificInput){
       alert("Please choose an answer for all questions!");
     }else if ((frontBackInput === "1" && coSpecificInput === "4") || (frontBackInput === "1" && coSpecificInput === "5")) {
+      $("#suggesterQuiz").hide();
       $("#cSharp").hide();
       $("#phpDrupal").hide();
       $("#rubyRails").hide();
       $("#moreInfo").hide();
-      $("#resultField").show();
+      $("#resultField").slideUp("slow");
       $("#cssDesign").show();
     }else if ((frontBackInput === "2" && appTypeInput === "2") || (frontBackInput === "2" && techTypeInput === "1") || (frontBackInput === "2" && coSpecificInput === "5")) {
+      $("#suggesterQuiz").hide();
       $("#cSharp").hide();
       $("#phpDrupal").hide();
       $("#cssDesign").hide();
       $("#moreInfo").hide();
-      $("#resultField").show();
+      $("#resultField").slideUp("slow");
       $("#rubyRails").show();
     }else if ((frontBackInput === "2" && coSpecificInput === "1") || (frontBackInput === "2" && techTypeInput === "2") || (techTypeInput === "2" && coSpecificInput === "1")) {
+      $("#suggesterQuiz").hide();
       $("#cssDesign").hide();
       $("#phpDrupal").hide();
       $("#rubyRails").hide();
       $("#moreInfo").hide();
-      $("#resultField").show();
+      $("#resultField").slideUp("slow");
       $("#cSharp").show();
     }else if ((appTypeInput === "2" && techTypeInput === "1") || (appTypeInput === "2" && coSpecificInput === "5")) {
+      $("#suggesterQuiz").hide();
       $("#cSharp").hide();
       $("#cssDesign").hide();
       $("#rubyRails").hide();
       $("#moreInfo").hide();
-      $("#resultField").show();
+      $("#resultField").slideUp("slow");
       $("#phpDrupal").show();
     }else {
+      $("#suggesterQuiz").hide();
       $("#cSharp").hide();
       $("#phpDrupal").hide();
       $("#rubyRails").hide();
