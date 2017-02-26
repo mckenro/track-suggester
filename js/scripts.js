4e9abcafe4def8ae23f0f9d98c7e1d37415ecdb1
Rob McKenzie <mckenro@gmail.com>
2017-02-26 09:22:47 -0800

    add updated conditional statements to clarify survey results

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 
     if (!userNameInput || !frontBackInput || !appTypeInput || !techTypeInput || !coTypeInput || !coSpecificInput){
       alert("Please choose an answer for all questions!");
-    }else if ((frontBackInput === "1" && coSpecificInput === "4") || (frontBackInput === "1" && coSpecificInput === "5")) {
+    }else if ((frontBackInput === "1" && coSpecificInput === "5") || (appTypeInput === "2" && coSpecificInput === "5")) {
       $("#suggesterQuiz").hide();
       $("#cSharp").hide();
       $("#phpDrupal").hide();
@@ -20,7 +20,7 @@ $(document).ready(function() {
       $("#moreInfo").hide();
       $("#cssDesign").show();
       $("#resultField").show();
-    }else if ((frontBackInput === "2" && appTypeInput === "2") || (frontBackInput === "2" && techTypeInput === "1") || (frontBackInput === "2" && coSpecificInput === "5")) {
+    }else if ((frontBackInput === "2" && coSpecificInput === "3") || (appTypeInput === "2" && coSpecificInput === "3")) {
       $("#suggesterQuiz").hide();
       $("#cSharp").hide();
       $("#phpDrupal").hide();
@@ -28,7 +28,7 @@ $(document).ready(function() {
       $("#moreInfo").hide();
       $("#resultField").show();
       $("#rubyRails").show();
-    }else if ((frontBackInput === "2" && coSpecificInput === "1") || (frontBackInput === "2" && techTypeInput === "2") || (techTypeInput === "2" && coSpecificInput === "1")) {
+    }else if ((frontBackInput === "2" && coSpecificInput === "1") || (techTypeInput === "2" && coSpecificInput === "1")) {
       $("#suggesterQuiz").hide();
       $("#cssDesign").hide();
       $("#phpDrupal").hide();
@@ -36,7 +36,7 @@ $(document).ready(function() {
       $("#moreInfo").hide();
       $("#resultField").show();
       $("#cSharp").show();
-    }else if ((appTypeInput === "2" && techTypeInput === "1") || (appTypeInput === "2" && coSpecificInput === "5")) {
+    }else if ((appTypeInput === "2" && coSpecificInput === "4") || (techTypeInput === "2" && coSpecificInput === "4")) {
       $("#suggesterQuiz").hide();
       $("#cSharp").hide();
       $("#cssDesign").hide();
